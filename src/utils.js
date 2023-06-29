68f52595e517607f273300783328aa53fdcd9627
adriano.dev <adriano.coelho@dataside.com.br>
2023-06-29 00:06:46 -0300

    del yarn.lock

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
 import Enum from "es6-enum";
+
 import { liquidificadorSucoDeManga } from "./protudos.js";
 
 export const FRUTAS = Enum("manga", "laranja", "uva", "acucarcristal");
 
 liquidificadorSucoDeManga(FRUTAS.manga, FRUTAS.acucarcristal);
+
+export const MARKETING = Enum("google_ads", "google_analitycs", "meta_ads");
